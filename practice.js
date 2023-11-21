07f17a7cc5da4c1d53fe8001010cbe2fad38fec1
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-11-21 14:28:40 +0600

    Practice problem added

--- practice.js
+++ practice.js
@@ -1,29 +1,16 @@
-// Show sum of 4 and 6
-
-const numbers = (num1, num2) => {
-  console.log(num1 + num2);
-};
-// numbers(4, 6);
-
-// arr = [2, 3, 4, 5]; Answer will be: 14
-
-const arr = [2, 3, 4, 5];
-
-for (let num of arr) {
-  let sum = 0;
-  sum += num;
-  // console.log(sum);
-}
-// console.log(sum);
-
-const arr2 = [1,23,4]
-
-const sumOfNum = (arr)=>{
-let s=0;
-for(let num of arr){
-  s += num;
-}
-return s;
-}
-
-console.log(sumOfNum(arr2));
+(() => {
+    let x, y;
+    try {
+      throw new Error();
+    } catch (x) {
+      x = 1;
+      y = 2;
+      console.log(x);
+    }
+    console.log(x);
+    console.log(y);
+  })();
+  
+// Output: 1
+//         undefined
+//         2
