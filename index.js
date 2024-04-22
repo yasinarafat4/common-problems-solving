24e45c71c186355a183be6fe8167cae50bf1806b
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-04-22 07:33:04 +0600

    new problen solved

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const num = [1,2,2,2,3,4,4]
 
 let uniqueNum = [...new Set(num)];
-console.log(uniqueNum);
+// console.log(uniqueNum);
+
+const number =(num1, num2)=>{
+    console.log(num1+num2);
+}
+
+number(2,4)
