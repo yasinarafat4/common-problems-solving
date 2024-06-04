2a03b841cdb8af8ed118d56c1428f774424be30f
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-06-04 21:05:44 +0600

    Problem solved

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const number =(num1, num2)=>{
 
 number(2,4)
 
+// Findout sum of 3,4,5
 
+function sum(num1, num2, num3) {
+  console.log(num1 + num2 + num3);
+}
+sum(3, 4, 5);
 
 // findout the unique number
 const num = [1,2,2,2,3,4,4]
