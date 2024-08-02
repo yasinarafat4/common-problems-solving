3f0008418b2b7988fcf9df2b72be52e3c8a0f753
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-08-03 01:08:24 +0600

    uncommented

--- practice.js
+++ practice.js
@@ -25,12 +25,12 @@
 
 // loop
 const arr = [1, 2, 3, 4, 5, 6];
-// console.log(arr[0]);
-// console.log(arr[1]);
-// console.log(arr[2]);
-// console.log(arr[3]);
-// console.log(arr[4]);
-// console.log(arr[5]);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
 
 for (let i = 0; i < arr.length; i++) {
   // console.log(arr[i]);
