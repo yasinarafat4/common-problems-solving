a53dd8d3cd09e175d40318b52f171e027b63661f
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-08-28 21:19:27 +0600

    problem solved

--- practice.js
+++ practice.js
@@ -46,10 +46,10 @@ for (let n of num) {
 }
 console.log(uniqueNum);
 
-// const FormatDate = (timestamp) => {
-//   const options = { year: "numeric", month: "short", day: "numeric" };
-//   return new Date(timestamp).toLocaleDateString("en-US", options);
-// };
+const FormatDate = (timestamp) => {
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  return new Date(timestamp).toLocaleDateString("en-US", options);
+};
 
-// const newDate = FormatDate(Date.now());
+const newDate = FormatDate(Date.now());
 // console.log(newDate);
