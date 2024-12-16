58be91cade6cfd1b799b9417ab437d9ebd51e580
yasinarafat4 <mdyasinarafathasib@gmail.com>
2024-12-16 23:50:41 +0600

    practice updated

--- practice.js
+++ practice.js
@@ -1,27 +1,27 @@
-// (() => {
-//     let x, y;
-//     try {
-//       throw new Error();
-//     } catch (x) {
-//       x = 1;
-//       y = 2;
-//       console.log(x);
-//     }
-//     console.log(x);
-//     console.log(y);
-//   })();
+(() => {
+    let x, y;
+    try {
+      throw new Error();
+    } catch (x) {
+      x = 1;
+      y = 2;
+      console.log(x);
+    }
+    console.log(x);
+    console.log(y);
+  })();
 
-// // Output: 1
-// //         undefined
-// //         2
+// Output: 1
+//         undefined
+//         2
 
-// const age = 100;
+const age = 100;
 
-// if (age >= 70) {
-//   console.log("He is Bura Manus");
-// } else {
-//   console.log("He is Jubok");
-// }
+if (age >= 70) {
+  console.log("He is Bura Manus");
+} else {
+  console.log("He is Jubok");
+}
 
 // loop
 const arr = [1, 2, 3, 4, 5, 6];
